e0da458f92a8b5658794d1b7b287fc98e734187c
AlexTLDR <alex.badragan@protonmail.com>
2023-11-02 18:54:07 +0100

    initialized cobra-cli

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "github.com/AlexTLDR/FileSync/cmd"
 
 func main() {
-
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please give one argument.")
-		return
-	}
-
-	path := arguments[1]
-
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		fmt.Println("Path does not exist!", err)
-	}
-
-	mode := fileInfo.Mode()
-	switch mode.IsDir() {
-	case true:
-		fmt.Println(path, "is a directory!")
-	case false:
-		fmt.Println(path, "is not a directory - and not a valid directory name!")
-	}
+	cmd.Execute()
 }
