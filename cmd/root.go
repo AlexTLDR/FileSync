42cd3ad203c6c8c020475a3196fcd350811244d3
AlexTLDR <alex.badragan@protonmail.com>
2023-11-03 13:16:22 +0100

    added the checkup to cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +22,29 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("test cobra")
+		arguments := os.Args
+		if len(arguments) == 1 {
+			fmt.Println("Please give one argument.")
+			return
+		}
+
+		path := arguments[1]
+
+		fileInfo, err := os.Stat(path)
+		if err != nil {
+			fmt.Println("Path does not exist!", err)
+		}
+
+		mode := fileInfo.Mode()
+		switch mode.IsDir() {
+		case true:
+			fmt.Println(path, "is a directory!")
+		case false:
+			fmt.Println(path, "is not a directory - and not a valid directory name!")
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +67,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
