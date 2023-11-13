868051d272520a8fde6923abfb96a846a96a11a4
AlexTLDR <alex.badragan@protonmail.com>
2023-11-13 21:45:32 +0100

    created a basic file compare

---
+++ exp/compare.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file1, err := os.Open("/home/alex/Git/FileSync/go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file1.Close()
+
+	file2, err := os.Open("/home/alex/Git/FileSync/go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+
+	content1, err := io.ReadAll(file1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content2, err := io.ReadAll(file2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if bytes.Equal(content1, content2) {
+		log.Println("Files are the same")
+	} else {
+		log.Println("Files are different")
+	}
+}
