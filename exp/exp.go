e1eaaaf600179e67e921644b4f0028f589bad5dc
AlexTLDR <alex.badragan@protonmail.com>
2023-11-13 20:53:03 +0100

    created a file scanner

--- exp/exp.go
+++ exp/exp.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 )
 
-func main() {
-	err := filepath.Walk(".",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			fmt.Println(path, info.Size())
-			return nil
-		})
+func visitFile(path string, info os.FileInfo, err error) error {
 	if err != nil {
-		log.Println(err)
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	if !info.IsDir() {
+		fmt.Println("File:", path)
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run scanner.go <directory>")
+		os.Exit(1)
+	}
+
+	root := os.Args[1]
+
+	fmt.Println("Scanning directory:", root)
+
+	err := filepath.Walk(root, visitFile)
+
+	if err != nil {
+		fmt.Println("Error:", err)
 	}
 }
