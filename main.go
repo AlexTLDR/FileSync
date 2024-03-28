eb096522c9ae1b82139f30faf7fe6dd4a5a2bc25
AlexTLDR <alex.badragan@protonmail.com>
2024-03-28 09:59:12 +0100

    switched to gocloud.dev/blob

--- main.go
+++ main.go
@@ -1,63 +1,145 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
-	"path/filepath"
+	"io"
 	"time"
+
+	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/fileblob"
+	_ "gocloud.dev/blob/memblob"
 )
 
-func syncDirs(srcDir, destDir string) error {
-	// Walk through the source directory
-	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
+func syncDirs(srcDir, destDir *blob.Bucket) error {
+
+	srcDirIt := srcDir.List(&blob.ListOptions{})
+	for {
+		obj, err := srcDirIt.Next(context.Background())
+		if err != nil && err != io.EOF {
 			return err
 		}
-
-		// Skip hidden files and directories
-		if info.Name()[0] == '.' {
-			return nil
+		if obj == nil {
+			break
 		}
+		fmt.Println(obj.Key)
+	}
+	/*
+		// Keep track of all files in the destination directory
+		destFiles := make(map[string]bool)
+		err := filepath.Walk(destDir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
 
-		// Get the relative path within the source directory
-		relPath, err := filepath.Rel(srcDir, path)
-		if err != nil {
-			return err
-		}
-
-		// Construct the destination path
-		destPath := filepath.Join(destDir, relPath)
-
-		// Check if the destination file exists
-		destInfo, err := os.Stat(destPath)
-		if os.IsNotExist(err) || info.ModTime().After(destInfo.ModTime()) {
-			// If the destination file doesn't exist or the source file is newer,
-			// create the destination file/directory
 			if info.IsDir() {
-				err = os.MkdirAll(destPath, 0755)
-			} else {
-				// Read the file content
-				data, err := os.ReadFile(path)
-				if err != nil {
+				return nil
+			}
+
+			relPath, err := filepath.Rel(destDir, path)
+			if err != nil {
+				return err
+			}
+
+			destFiles[relPath] = false
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+		// Walk through the source directory
+		err = filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// Skip hidden files and directories
+			if info.Name()[0] == '.' {
+				return nil
+			}
+
+			// Get the relative path within the source directory
+			relPath, err := filepath.Rel(srcDir, path)
+			if err != nil {
+				return err
+			}
+
+			// Construct the destination path
+			destPath := filepath.Join(destDir, relPath)
+
+			// Check if the destination file exists
+			destInfo, err := os.Stat(destPath)
+			if os.IsNotExist(err) || info.ModTime().After(destInfo.ModTime()) {
+				// If the destination file doesn't exist or the source file is newer,
+				// create the destination file/directory
+				if info.IsDir() {
+					err = os.MkdirAll(destPath, 0755)
+				} else {
+					// Read the content
+					data, err := os.ReadFile(path)
+					if err != nil {
+						return err
+					}
+
+					// Write the content to the destination file
+					err = os.WriteFile(destPath, data, 0644)
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+			// Mark the file as updated
+			destFiles[relPath] = true
+			return nil
+		})
+
+		if err != nil {
+			return err	srcDirIt := srcDir.List(&blob.ListOptions{})
+			for {
+				obj, err := srcDirIt.Next(context.Background())
+				if err != nil && err != io.EOF {
 					return err
 				}
+				if obj == nil {
+					break
+				}
+				fmt.Println(obj.Key)
+			}
+		}
 
-				// Write the content to the destination file
-				err = os.WriteFile(destPath, data, 0644)
+		// Delete files in the destination directory that weren't updated
+		for relPath, updated := range destFiles {
+			if !updated {
+				err = os.Remove(filepath.Join(destDir, relPath))
 				if err != nil {
 					return err
 				}
 			}
 		}
+	*/
+	return nil
 
-		return nil
-	})
 }
 
 func main() {
 	// Replace with your actual source and destination folder paths
-	dir1 := "test-data/dir1"
-	dir2 := "test-data/dir2"
+	// dir1 := "test-data/dir1"
+	//dir2 := "test-data/dir2"
+	ctx := context.Background()
+	dir1, err := blob.OpenBucket(ctx, "file:///home/alex/git/FileSync/test-data/dir1")
+	if err != nil {
+		panic(err) //
+	}
+	defer dir1.Close()
+
+	dir2, err := blob.OpenBucket(ctx, "file:///home/alex/git/FileSync/test-data/dir2")
+	if err != nil {
+		panic(err) //
+	}
+	defer dir2.Close()
 
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
