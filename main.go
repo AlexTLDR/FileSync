a8a20cfaba6b76f5d66fc7e91a2375d97f672804
AlexTLDR <alex.badragan@protonmail.com>
2024-08-22 16:43:03 +0200

    not handling properly the isDeleted tag in the bucket

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -17,68 +16,92 @@ import (
 	"google.golang.org/api/option"
 )
 
-type foo struct {
+type fileStatus struct {
 	isDeleted bool
 	deletedOn *time.Time
-	obj       blob.ListObject
+	obj       *blob.ListObject
 }
 
-func syncDirToBucket(ctx context.Context, dir *blob.Bucket, bucket *blob.Bucket) error {
+const timeThreshold = 10 * time.Second
+
+func syncDirToBucket(ctx context.Context, dir *blob.Bucket, bucket *blob.Bucket) {
+	localFiles := make(map[string]*fileStatus)
+	bucketFiles := make(map[string]*fileStatus)
+
 	for {
-		localFiles := make(map[string]*blob.ListObject)
-		bucketFiles := make(map[string]*blob.ListObject)
+		log.Println("Starting synchronization cycle")
 
-		// List local files
-		if err := listFiles(ctx, dir, localFiles); err != nil {
-			return err
-		}
+		log.Println("Updating local file statuses")
+		updateFileStatuses(ctx, dir, localFiles)
+		log.Println("Updating bucket file statuses")
+		updateFileStatuses(ctx, bucket, bucketFiles)
 
-		// List bucket files
-		if err := listFiles(ctx, bucket, bucketFiles); err != nil {
-			return err
-		}
+		for key, localStatus := range localFiles {
+			bucketStatus, existsInBucket := bucketFiles[key]
 
-		// Sync local to bucket
-		for key, localObj := range localFiles {
-			bucketObj, existsInBucket := bucketFiles[key]
+			log.Printf("Processing file: %s", key)
+			localUTCTime := localStatus.obj.ModTime.UTC()
+			log.Printf("Local status: isDeleted=%v, modTime=%v (UTC)", localStatus.isDeleted, localUTCTime)
 			if existsInBucket {
-				if localObj.ModTime.After(bucketObj.ModTime) {
-					if err := uploadFile(ctx, dir, bucket, key); err != nil {
-						return err
-					}
-				} else if bucketObj.ModTime.After(localObj.ModTime) {
-					if err := downloadFile(ctx, bucket, key, filepath.Join("/home/alex/git/FileSync/test-data/dir1", key)); err != nil {
-						return err
-					}
-				}
-				delete(bucketFiles, key)
+				log.Printf("Bucket status: isDeleted=%v, modTime=%v", bucketStatus.isDeleted, bucketStatus.obj.ModTime)
 			} else {
+				log.Println("File does not exist in bucket")
+			}
+
+			if localStatus.isDeleted && !existsInBucket {
+				log.Printf("File %s deleted locally and doesn't exist in bucket, removing from tracking", key)
+				delete(localFiles, key)
+			} else if localStatus.isDeleted && existsInBucket && !bucketStatus.isDeleted {
+				log.Printf("Deleting file %s from bucket", key)
+				if err := bucket.Delete(ctx, key); err != nil {
+					log.Printf("Error deleting file %s from bucket: %v", key, err)
+				}
+			} else if !existsInBucket {
+				log.Printf("Uploading new local file %s to bucket", key)
 				if err := uploadFile(ctx, dir, bucket, key); err != nil {
-					return err
+					log.Printf("Error uploading file %s: %v", key, err)
+				}
+			} else if bucketStatus.isDeleted && !localStatus.isDeleted {
+				log.Printf("Deleting local file %s", key)
+				localPath := filepath.Join("/home/alex/git/FileSync/test-data/dir1", key)
+				if err := os.Remove(localPath); err != nil && !os.IsNotExist(err) {
+					log.Printf("Error deleting local file %s: %v", key, err)
+				}
+				localStatus.isDeleted = true
+				localStatus.deletedOn = bucketStatus.deletedOn
+			} else if !localStatus.isDeleted && !bucketStatus.isDeleted {
+				timeDiff := localUTCTime.Sub(bucketStatus.obj.ModTime)
+				if timeDiff > timeThreshold {
+					log.Printf("Uploading newer local file %s to bucket", key)
+					if err := uploadFile(ctx, dir, bucket, key); err != nil {
+						log.Printf("Error uploading file %s: %v", key, err)
+					}
+				} else if timeDiff < -timeThreshold {
+					log.Printf("Downloading newer bucket file %s", key)
+					if err := downloadFile(ctx, bucket, key, filepath.Join("/home/alex/git/FileSync/test-data/dir1", key)); err != nil {
+						log.Printf("Error downloading file %s: %v", key, err)
+					}
+				} else {
+					log.Printf("File %s is up to date (time difference: %v)", key, timeDiff)
+				}
+			}
+		}
+		for key, bucketStatus := range bucketFiles {
+			if _, existsLocally := localFiles[key]; !existsLocally && !bucketStatus.isDeleted {
+				log.Printf("Downloading new bucket file %s", key)
+				if err := downloadFile(ctx, bucket, key, filepath.Join("/home/alex/git/FileSync/test-data/dir1", key)); err != nil {
+					log.Printf("Error downloading new file %s: %v", key, err)
 				}
 			}
 		}
 
-		// Delete files from bucket that don't exist locally
-		for key := range bucketFiles {
-			if err := bucket.Delete(ctx, key); err != nil {
-				return err
-			}
-		}
-
-		// Delete local files that don't exist in bucket
-		for key := range bucketFiles {
-			localPath := filepath.Join("/home/alex/git/FileSync/test-data/dir1", key)
-			if err := os.Remove(localPath); err != nil && !os.IsNotExist(err) {
-				return err
-			}
-		}
-
-		time.Sleep(5 * time.Second)
+		log.Println("Synchronization cycle completed")
+		time.Sleep(timeThreshold)
 	}
 }
+func updateFileStatuses(ctx context.Context, b *blob.Bucket, files map[string]*fileStatus) {
+	currentFiles := make(map[string]bool)
 
-func listFiles(ctx context.Context, b *blob.Bucket, files map[string]*blob.ListObject) error {
 	it := b.List(&blob.ListOptions{})
 	for {
 		obj, err := it.Next(ctx)
@@ -86,13 +109,32 @@ func listFiles(ctx context.Context, b *blob.Bucket, files map[string]*blob.ListO
 			break
 		}
 		if err != nil {
-			return err
+			log.Printf("Error listing files: %v", err)
+			return
 		}
 		if !obj.IsDir && !strings.HasPrefix(obj.Key, ".") {
-			files[obj.Key] = obj
+			currentFiles[obj.Key] = true
+			if status, exists := files[obj.Key]; exists {
+				if status.isDeleted {
+					// File was previously deleted but now exists again
+					status.isDeleted = false
+					status.deletedOn = nil
+				}
+				status.obj = obj
+			} else {
+				files[obj.Key] = &fileStatus{obj: obj, isDeleted: false}
+			}
 		}
 	}
-	return nil
+
+	// Mark files as deleted if they no longer exist
+	for key, status := range files {
+		if !currentFiles[key] && !status.isDeleted {
+			status.isDeleted = true
+			now := time.Now()
+			status.deletedOn = &now
+		}
+	}
 }
 
 func uploadFile(ctx context.Context, dir *blob.Bucket, bucket *blob.Bucket, key string) error {
@@ -168,8 +210,5 @@ func main() {
 	defer bucketHandle.Close()
 
 	// Sync the local directory to the bucket
-	err = syncDirToBucket(ctx, dir1, bucketHandle)
-	if err != nil {
-		fmt.Println("Error syncing directory to bucket:", err)
-	}
+	syncDirToBucket(ctx, dir1, bucketHandle)
 }
